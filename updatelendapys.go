8ed62d29e313f959227eb33cfac5a2a4da0201c1
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-11 18:43:25 +0800

    [fix] update lend apys become one function

--- updatelendapys.go
+++ updatelendapys.go
@@ -6,37 +6,10 @@ import (
 	"github.com/0xVanfer/ethaddr"
 )
 
-// Update all the protocol's lend pools apys.
-func (prot *Protocol) UpdateLendApys() error {
-	// protocol basic must be initialized
-	if !prot.ProtocolBasic.Regularcheck() {
-		return errors.New("protocol basic must be initialized")
-	}
-	switch prot.ProtocolBasic.ProtocolName {
-	// aave v2
-	case ethaddr.AaveV2Protocol:
-		return prot.updateAaveV2Lend([]string{})
-	// aave v3
-	case ethaddr.AaveV3Protocol:
-		return prot.updateAaveV3Lend([]string{})
-	// benqi
-	case ethaddr.BenqiProtocol:
-		return prot.updateBenqiLend([]string{})
-	case ethaddr.CompoundProtocol:
-		// todo
-		return nil
-	case ethaddr.TraderJoeProtocol:
-		// todo
-		return nil
-	default:
-		return errors.New("protocol not supported lend pools")
-	}
-}
-
 // Update some of the protocol's lend pools apys by given underlying addresses.
 //
-// If "underlyings" is empty, equal to UpdateLendApys().
-func (prot *Protocol) UpdateLendApyByUnderlying(underlyings []string) error {
+// If "underlyings" is empty, update all pools.
+func (prot *Protocol) UpdateLendApys(underlyings ...string) error {
 	// protocol basic must be initialized
 	if !prot.ProtocolBasic.Regularcheck() {
 		return errors.New("protocol basic must be initialized")
@@ -51,12 +24,13 @@ func (prot *Protocol) UpdateLendApyByUnderlying(underlyings []string) error {
 	// benqi
 	case ethaddr.BenqiProtocol:
 		return prot.updateBenqiLend(underlyings)
+	// compound
 	case ethaddr.CompoundProtocol:
 		// todo
 		return nil
+	// tradejoe
 	case ethaddr.TraderJoeProtocol:
-		// todo
-		return nil
+		return prot.updateTraderjoeLend(underlyings)
 	default:
 		return errors.New("protocol not supported lend pools")
 	}
